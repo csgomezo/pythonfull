889672d2bf587194a57192078252fb0f91398d28
csgomezo <carloss.gomezo@utadeo.edu.co>
2021-07-29 17:18:44 -0500

    First Commit with Solvo Assesment

---
+++ PruebaSolvo.py
@@ -0,0 +1,5 @@
+# players
+#A 2-9 J Q K    C D T P (Cannot repeat number or type) (AS 1 or 11)
+#Consult if want other card Else continue
+#calculate the game score print all players
+#Print the winner
